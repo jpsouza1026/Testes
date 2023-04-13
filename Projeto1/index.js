7a15ec8d3542ae4687b169a1e3a7e8ef3567b41f
jpsouza1026 <129884828+jpsouza1026@users.noreply.github.com>
2023-04-13 18:08:47 +0000

    projeto inicial

---
+++ Projeto1/index.js
@@ -0,0 +1,4 @@
+
+console.log("Olá, mundo! eu sou José");
+
+console.log("Servidor rodando na porta 3000");
--- web-server/index.js
+++ web-server/index.js
@@ -1,1 +1,11 @@
-console.log("Hello IoT")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Olá, mundo! eu sou José");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
